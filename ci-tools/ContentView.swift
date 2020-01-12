94a1fd05d1e715387f358909e9822df62c2e79f6
Tibor Bödecs <mail.tib@gmail.com>
2020-01-12 13:01:29 +0800

    Initial Commit

---
+++ ci-tools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ci-tools
+//
+//  Created by Tibor Bodecs on 2020. 01. 12..
+//  Copyright © 2020. Tibor Bodecs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
