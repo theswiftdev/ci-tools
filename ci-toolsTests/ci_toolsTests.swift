77c8862c1c592d55976269c79c85b20606c73271
Tibor Bödecs <mail.tib@gmail.com>
2020-01-12 13:23:24 +0800

    test coverage

---
+++ ci-toolsTests/ci_toolsTests.swift
@@ -0,0 +1,18 @@
+//
+//  ci_toolsTests.swift
+//  ci-toolsTests
+//
+//  Created by Tibor Bodecs on 2020. 01. 12..
+//  Copyright © 2020. Tibor Bodecs. All rights reserved.
+//
+
+import XCTest
+import SwiftUI
+@testable import ci_tools
+
+class ci_toolsTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertTrue(ContentView().body is Text)
+    }
+}
